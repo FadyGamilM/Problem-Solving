4cad2b88241e3a46ffaec4271a305d63e19febca
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-07-20 14:42:52 +0300

    FIX BUG | fixing the 2-sum problem soln

--- 001_two-sum-problem/main.go
+++ 001_two-sum-problem/main.go
@@ -3,13 +3,54 @@ package main
 import "fmt"
 
 func main() {
-	inp := []int{1, 3, 9, 3, 5}
-	target := 14
+	inp := []int{3, 2, 4}
+	target := 6
 	fmt.Println("O(n*n) solution")
 	fmt.Println(trivial_solution(inp, target))
 	fmt.Println("O(n) solution")
 	fmt.Println(optimized_solution(inp, target))
 }
+
+// inp := []int{1, 3, 9, 3, 5}
+func optimized_solution(inp []int, target int) []int {
+	if len(inp) == 0 || len(inp) == 1 {
+		return nil
+	}
+
+	store := make(map[int]int)
+	num_to_find := 0
+
+	for idx, val := range inp {
+		_, exists := store[inp[idx]]
+		num_to_find = target - val
+		if exists {
+			// already exists, so return the result
+			return []int{store[val], idx}
+		} else {
+			store[num_to_find] = idx
+		}
+	}
+	return nil
+}
+
+// [3,2,4], target = 6
+
+// num_to_find = 0
+// store = {}
+
+// idx = 0, val = 3
+// ntf = 3
+// store = {[3, 0]}
+
+// val = 2
+// ntf = 4
+// store = {[3, 0], [4, 1]}
+
+// val = 4
+// ntf = 4
+// store = {[3, 0], [4, 1]}
+
+// return []int{0, 1}
 
 // space complexity = O(1)
 func trivial_solution(inp []int, target int) []int {
@@ -22,27 +63,6 @@ func trivial_solution(inp []int, target int) []int {
 			if inp[j] == target-inp[i] {
 				return []int{i, j}
 			}
-		}
-	}
-	return nil
-}
-
-//	inp := []int{1, 3, 9, 3, 5}
-func optimized_solution(inp []int, target int) []int {
-	if len(inp) == 0 || len(inp) == 1 {
-		return nil
-	}
-	store := make(map[int]int) // {}
-	var num_to_find int        // = 0
-	for idx, val := range inp {
-		num_to_find = target - val // = 13
-		// check if this value is the num_to_find for any other number in the given array
-		if store[val] == 0 {
-			// its not visited before
-			// so store it in the map
-			store[num_to_find] = idx
-		} else {
-			return []int{store[val], idx}
 		}
 	}
 	return nil
